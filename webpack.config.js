3cd1183492b6c97a575d6bae659e3b1116e2e888
sdedieu <dedieu.sylvain38@gmail.com>
2022-01-30 19:10:37 +0100

    gain of time putting the commented code

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-module.exports = options => {
+module.exports = (options) => {
   return {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-      filename: 'bundle.js',   
-      publicPath: 'auto',
-      uniqueName: "mfe4"
+      filename: "bundle.js",
+      publicPath: "auto",
+      uniqueName: "mfe4",
     },
     module: {
       rules: [
         {
           test: /\.(ico)$/,
           exclude: /node_modules/,
-          use: ['file-loader'] // ?name=[name].[ext] is only necessary to preserve the original file name
+          use: ["file-loader"], // ?name=[name].[ext] is only necessary to preserve the original file name
         },
         {
           test: /\.(jpg|png)$/,
@@ -28,36 +28,45 @@ module.exports = options => {
           exclude: /node_modules/,
           use: [
             {
-              loader: 'babel-loader',
+              loader: "babel-loader",
               options: {
                 cacheDirectory: true,
-                presets: ['@babel/react', '@babel/env']
-              }
+                presets: ["@babel/react", "@babel/env"],
+              },
             },
           ],
         },
-       {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader", "postcss-loader",
-          ],
-      }, 
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
       ],
     },
     plugins: [
       new MiniCssExtractPlugin({
         filename: "index.css",
-        chunkFilename: "index.css"
+        chunkFilename: "index.css",
       }),
       new htmlWebpackPlugin({
         template: path.resolve(__dirname, "public", "index.html"),
         favicon: "./public/favicon.ico",
       }),
+      /*
+      new ModuleFederationPlugin({
+        // For remotes (please adjust)
+        name: "mfe",
+        library: { type: "var", name: "mfe" },
+        filename: "remoteEntry.js",
+        exposes: {
+          "./module": ".//src/main.js",
+        },
+        shared: ["react", "react-dom", "axios"],
+      }),
+      */
     ],
     devServer: {
       port: 4203,
-      historyApiFallback: true
-    }
-  }
-}
+      historyApiFallback: true,
+    },
+  };
+};
